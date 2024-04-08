3c352687c4fe8c3c3c0767c2a69bcf7728eddcde
Joashane <joashanegarrick@outlook.com>
2024-04-08 09:45:42 -0500

    updated javascript and add lambda code to the backend folder

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://hy34juywnprinehvy6csavieqi0xoutc.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://4uwglr33otfh2izfxvkhlkssgy0qtlds.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
